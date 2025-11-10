c9d9315b41e89ce5ce12db1a6211838accc2af77
Lakshmanh00 <hlakshman89@gmail.com>
2025-11-10 16:30:43 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,18 @@
+addition
+sum=a+b
+print("adition is",sum)
+#d=int(input("enter the number 1"))
+b=int(input("enter the number 2"))
+#aivision
+div1=a/b
+div2=a//b # floor division
+div3=a%b #modolus
+print("division is:",div1)
+print("floor division is:",div2)
+print("modolus is:",div3)
+#multiplication
+mul=a*b
+print("multiplication is:",mul)
+#substraction
+sub=a-b
+print("subtraction is:",sub)
