2af8e8a95c55526d9525f4a7696dcc8fc17abb34
Lakshmanh00 <hlakshman89@gmail.com>
2025-11-12 10:18:02 +0530

    Create fromatted string.py

---
+++ fromatted string.py
@@ -0,0 +1,6 @@
+#formatted string
+name="lakshman"
+age="18"
+college="shridevi"
+department="aiml"
+print(f"my name is :{name}and my age is:{age}and my college is:{college}and my department is:{department}")
