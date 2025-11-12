cd837562dc466238d55cac9712a7ae82a6127bf2
Lakshmanh00 <hlakshman89@gmail.com>
2025-11-12 15:33:31 +0530

    Create user id.py

---
+++ user id.py
@@ -0,0 +1,13 @@
+def userid(a,b):
+  name=input("enter your name")
+  age=int(input("enter the age"))
+  rollnumber=int(input("enter the rollnumber"))
+  return name,age,rollnumber
+ 
+  addition=age+rollnumber
+  print("the addition is",addition)
+  mul=a*b
+  print("the multiplication is",mul)
+  return name,age,rollnumber
+userid(3,4)
+   
